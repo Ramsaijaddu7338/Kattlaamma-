42eb15afc917dea8082c47346126da07f3e0e5a4
Ramsaijaddu7338 <ramsai.bbcl7@gmail.com>
2026-08-10 01:37:18 +0530

    Update API_URL in configuration

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // ===== CONFIGURATION =====
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbzJzU-ZC6t3cBpr2FOGg-dX7yfND3xUNT4yWNo8mLtJYnD4xo-wf5LdUDdO1C5mnHR5/exec', // ← REPLACE WITH YOUR DEPLOYMENT URL
+    API_URL: 'https://script.google.com/macros/s/AKfycbxrEMAT7AUpT5lnapHZspzDyjZ-jLl0PY9X8wbVm8ocupxbUkqSpEvbCDm-MWMCNaS4/exec', // ← REPLACE WITH YOUR DEPLOYMENT URL
     GOOGLE_DRIVE_FOLDER: '1WUeWMSHjLL5hadKdplou3OfyH95UB9aj',
     SPREADSHEET_ID: '1pawm12qQ6KA43CUnrgMbHtJlRgcEHOQ8_-kzk_iLiwM',
     TEMPLE_NAME: 'శ్రీ శ్రీ కట్లమ్మ అమ్మవారి ఆలయం',
